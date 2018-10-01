86e19468d243c1895e059beec190b4af319549f1
PrathamDogra <43649218+PrathamDogra@users.noreply.github.com>
2018-10-01 11:59:47 +0530

    Asking user
    Asking user if he want to take another factorial.

--- main.c
+++ main.c
@@ -14,11 +14,18 @@ int fact(int num)
 int main()
 {
    int i,n;
+    char ch;
+    do
+    {
    printf("enter the number: ");
    scanf("%d",&n);
 
    i=fact(n);
+    printf("\nThe Factorial of the number is:");
    printf("%d",i);
+   printf("\nWant to use it again");
+    scanf("%c",&ch);
+    }while(ch=='y'||ch=='Y')
 
 
     return 0;
